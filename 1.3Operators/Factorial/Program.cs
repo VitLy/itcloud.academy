38bbba114ac8022443dc662d98608b04a27a6a8e
Vitaly Borischak <borischak.vd@gmail.com>
2017-12-17 01:03:04 +0200

    second commit:Factorial

--- 1.3Operators/Factorial/Program.cs
+++ 1.3Operators/Factorial/Program.cs
@@ -10,42 +10,35 @@ namespace Factorial
     {
         static void Main(string[] args)
         {
-            int a = 0;
+            long a;
+            bool type;
             do
             {
-                try
-                {
-                    Console.WriteLine("Please, type the integer number from 1 to 20");
-                    a = Int32.Parse(Console.ReadLine());
-                }
-                catch
-                {
-                    a = 0;
-                    break;
-                }
+                Console.WriteLine("Please, type the integer number from 1 to 20");
+                type = Int64.TryParse(Console.ReadLine(), out a);
             }
             while (a < 1 || a > 20);
-            Console.WriteLine("{0}!={1}",a,Factorial(a));
-            Console.WriteLine("Press any key");
-            Console.ReadKey();
-        }
-        public static long Factorial(long _a)
-        {
-            long _rez = _a;
-            if (_a == 0) return 1;
-            else
-            {
-                for (int i = 1; i <= _a-1; i++)
-                {
-                    _rez = i * _rez;
-                }
-                return _rez;
+                Console.WriteLine("{0}!={1}", a, Factorial(a));
+                Console.WriteLine("Press any key");
+                Console.ReadKey();
             }
+        public static long Factorial(long a)
+            {
+                long rez = a;
+                if (a == 0) return 1;
+                else
+                {
+                    for (int i = 1; i <= a - 1; i++)
+                    {
+                        rez = i * rez;
+                    }
+                    return rez;
+                }
+            }
+
+
         }
-
-
     }
-}
 
 
 
